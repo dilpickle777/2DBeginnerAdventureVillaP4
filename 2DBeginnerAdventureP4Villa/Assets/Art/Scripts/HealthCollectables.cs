04a3ea56b69087550e4e221866ff9ea11077ed38
dilpickle777 <727696@student.dusd.net>
2024-11-07 11:39:51 -0800

    ADDED A ememy controller
    added a enemy controller so it can move by it self and damage the person playing

--- 2DBeginnerAdventureP4Villa/Assets/Art/Scripts/HealthCollectables.cs
+++ 2DBeginnerAdventureP4Villa/Assets/Art/Scripts/HealthCollectables.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectables : MonoBehaviour
+public class HealthCollectables: MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
         PlayerController controller = other.GetComponent<PlayerController>();
         if (controller != null)
         {
-            if(controller.health < controller.maxHealth)
-            {
-                controller.ChangeHealth(1);
+                controller.ChangeHealth(5);
                 Destroy(gameObject);
-            }
         }
             
 
